070d595fa7c30aa83a4420760b57bfec48a3316f
chayanika128 <chayanikapatra723@gmail.com>
2026-02-11 23:13:24 +0530

    split_join.py

---
+++ split_join.py
@@ -0,0 +1,13 @@
+a= "my name is chayanika"
+b=a.split()
+print(b)
+c=" ".join(b)
+print(c)
+
+
+
+sentence="i am chayanika"
+words=sentence.split()
+words.reverse()
+reversed_sentence=" ".join(words)
+print(reversed_sentence)
